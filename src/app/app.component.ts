a8afbf99c68874fb193e9855cc58d54ccce1a1c2
Ankur <ankur.akvaliya@acearea.com>
2023-05-16 11:16:43 +0530

    angular pwa tutorial

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {SwPush, SwUpdate} from "@angular/service-worker";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  implements OnInit {
+
+
+    constructor(private swUpdate: SwUpdate) {
+
+    }
+
+    ngOnInit() {
+      if(this.swUpdate.isEnabled){
+
+        this.swUpdate.available.subscribe(()=>{
+          if(confirm("New vsersion Available. Load new Version?")){
+            window.location.reload();
+          }
+        });
+      }
+
+
+    }
+
+}
+
